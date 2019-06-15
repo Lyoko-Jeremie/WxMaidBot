6b1d8f6c886a755f9b529f23091856a10dd46d86
Jeremie <lucheng989898@gmail.com>
2019-06-15 20:27:53 +0800

    add HundredFamilyNameMap add hash-list mode (the map with list)

--- src/ChineseNameAnalysis.ts
+++ src/ChineseNameAnalysis.ts
@@ -1,12 +1,30 @@
 import {HundredFamilyNameDist} from './HundredFamilyNameDist';
 import XRegexp from 'xregexp';
-import {split as _split, reduce, isNil, concat} from 'lodash';
+import {split as _split, reduce, isNil, concat, isEqual, slice, find} from 'lodash';
 
 const HundredFamilyNameList = _split(HundredFamilyNameDist, '|');
 const HundredFamilyNameSingles = HundredFamilyNameList.filter(T => T.length == 1);
 const HundredFamilyNameDoubles = HundredFamilyNameList.filter(T => T.length == 2);
 const HundredFamilyNameOthers = HundredFamilyNameList.filter(T => T.length > 2);
 const HundredFamilyNameErrors = HundredFamilyNameList.filter(T => T.length == 0);
+
+const HundredFamilyNameMap = (() => {
+    let map = new Map<string, string[][]>();
+    HundredFamilyNameList.forEach(T => {
+        let s = Array.from(T);
+        let it: string[][] | undefined = map.get(s[0]);
+        if (it) {
+            it.push(s);
+            map.set(s[0], it);
+            // have double '郁'
+            // console.error(s[0], s);
+            // throw new Error("HundredFamilyNameMap failed...");
+        } else {
+            map.set(s[0], [s]);
+        }
+    });
+    return map;
+})();
 
 // for debug
 // console.log('HundredFamilyNameSingles');
@@ -30,6 +48,30 @@ export function findIndexOfSingleNameChar(s: string): number[] {
     const sA = Array.from(s);
     return reduce(sA, (Acc: number[], T, I) => {
         return !isNil(HundredFamilyNameSingles.find(it => T === it)) ? concat(Acc, [I]) : Acc;
+    }, []);
+}
+
+
+export function testHaveSingleNameCharAll(s: string): number {
+    const sA = Array.from(s);
+    return reduce(sA, (Acc, T, I) => {
+        return HundredFamilyNameMap.has(T) ? Acc + 1 : Acc;
+    }, 0);
+}
+
+export function findIndexOfSingleNameCharAll(s: string): { i: number, n: number }[] {
+    const sA = Array.from(s);
+    return reduce(sA, (Acc: { i: number, n: number }[], T, I) => {
+        let N: string[][] | undefined = HundredFamilyNameMap.get(T);
+        if (!isNil(N)) {
+            let f = find(N, nt => isEqual(nt, slice(sA, I, I + nt.length)));
+            if (!isNil(f)) {
+                return concat(Acc, [{i: I, n: f.length}]);
+            } else {
+                return Acc;
+            }
+        }
+        return Acc;
     }, []);
 }
 
